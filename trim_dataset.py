6abc7f9777778d4a3a92b7ddf6b1917abb7251cd
G. Barreto <81831041+Barreto-G@users.noreply.github.com>
2025-01-07 17:37:39 -0300

    resized images and corrected bugs
    Corrigidos bugs relacionados a leitura e escrita dos arquivos JSON, além de adicionar uma função de downscale das fotos para otimizar o treinamento do modelo

--- trim_dataset.py
+++ trim_dataset.py
@@ -1,7 +1,7 @@
 import json, os, cv2
 from image import Image
 import albumentations as A
-import uuid
+import numpy as np
 
 class TrimDataset:
     def __init__(self, base_dir):
@@ -35,12 +35,29 @@ class TrimDataset:
             img.content = cv2.imread(img_path)
             if img.content is None:
                 print(f"Erro ao ler imagem no caminho: {img_path}")
+                print(f"ID da categoria:{img.category_id}; nome da categoria:{category_name}")
+                print(f"IMG DATA:{img_data}")
+                print(f"ANNOTATIONS:{ann}")
+                continue
+            else:
+                rle_mask = ann['segmentation']['counts']
+                img.mask = img._rle_to_mask(rle_mask, (img_data['height'], img_data['width']))
+                img.bbox = ann['bbox']
+                img.file_name = img_name
 
-            rle_mask = ann['segmentation']['counts']
-            img.mask = img._rle_to_mask(rle_mask, (img_data['height'], img_data['width']))
-            img.bbox = ann['bbox']
-            img.file_name = img_name
-            self.images.append(img)
+                if img.content.shape[:2] == [224,224]:
+                    self.images.append(img)
+                else:
+                    # Redimensionando as fotos
+                    resized_image = self.resize_image(img)
+                    self.images.append(resized_image)
+                    img_data['width'] = resized_image.content.shape[0]
+                    img_data['height'] = resized_image.content.shape[1]
+                    ann['size'] = resized_image.content.shape[:2]
+                    ann['bbox'] = resized_image.bbox
+                    ann['area'] = resized_image.bbox[2] * resized_image.bbox[3]
+                    ann['segmentation']['counts'] = resized_image._mask_to_rle(resized_image.mask)
+            
 
     def _img_augmentation(self, image, angle=15):
         transform = A.Compose([
@@ -65,7 +82,7 @@ class TrimDataset:
         for image in copy_images:
             for i in range(2):
                 augmented_img = self._img_augmentation(image)
-                augmented_img_name = f'{image.file_name[:-4]}-augmented-{uuid.uuid4().hex[:4]}.png'
+                augmented_img_name = f'{image.file_name[:-4]}-augmented-{i}.png'
                 aux = Image(name=augmented_img_name, 
                                       bbox=augmented_img['bbox'],
                                       mask= augmented_img['mask'],
@@ -78,7 +95,11 @@ class TrimDataset:
 
     def add_json(self, image: Image, json_file= None):
         segmentation = image._mask_to_rle(image.mask)
-        img_id = len(self.images)
+        if json_file == None:
+            img_id = len(self.images)
+        else:
+            img_id = len(json_file['images'])+1
+
         annotation = {
             'id': img_id,
             'image_id': img_id,
@@ -86,14 +107,14 @@ class TrimDataset:
             'area': image.bbox[2] * image.bbox[3],
             'bbox': image.bbox,
             'iscrowd': 0,
-            'segmentation': {'counts': segmentation, 'size': [2112, 2112]}
+            'segmentation': {'counts': segmentation, 'size': [image.content.shape[0], image.content.shape[1]]}
         }
 
         image_info = {
             'id': img_id,
             'file_name': image.file_name,
-            'width': 2112,
-            'height': 2112
+            'width': image.content.shape[0],
+            'height': image.content.shape[0]
         }
         if json_file == None:
             self.json['annotation'].append(annotation)
@@ -147,6 +168,49 @@ class TrimDataset:
             path = os.path.join(path, img.file_name)
             cv2.imwrite(path, img.content)
     
+    def split_dataset(self, train_ratio=0.8, eval_ratio=0.1, test_ratio=0.1):
+        num_images = len(self.images)
+        num_train = int(train_ratio * num_images)
+        num_val = int(eval_ratio * num_images)
+
+        indices = np.random.permutation(num_images)
+        self.data_split['train'] = indices[:num_train]
+        self.data_split['val'] = indices[num_train:num_train + num_val]
+        self.data_split['test'] = indices[num_train + num_val:]
+
+
+    def resize_image(self, image:Image, target_size=(224, 224)):
+        """
+        Redimensiona uma imagem e ajusta sua bounding box proporcionalmente ao novo tamanho.
+        
+        :param image: Imagem original (lida com cv2.imread()).
+        :param bbox: Bounding box original no formato [x_min, y_min, x_max, y_max].
+        :param target_size: Tamanho de destino da imagem (largura, altura).
+        :return: Imagem redimensionada e bounding box ajustada.
+        """
+        # Obtém as dimensões originais da imagem
+        original_height, original_width = image.content.shape[:2]
+        target_width, target_height = target_size
+
+        # Redimensiona a imagem
+        resized_image = cv2.resize(image.content, (target_width, target_height))
+        resized_mask = cv2.resize(image.mask, (target_width, target_height))
+
+        # Calcula os fatores de escala
+        scale_x = target_width / original_width
+        scale_y = target_height / original_height
+
+        # Ajusta as coordenadas da bounding box
+        x_min, y_min, x_max, y_max = image.bbox
+        x_min_new = int(x_min * scale_x)
+        y_min_new = int(y_min * scale_y)
+        x_max_new = int(x_max * scale_x)
+        y_max_new = int(y_max * scale_y)
+
+        resized_bbox = [x_min_new, y_min_new, x_max_new, y_max_new]
+
+        return Image(name=image.file_name, bbox=resized_bbox, category=image.category_id, content=resized_image, mask=resized_mask)
+
     def __repr__(self):
         return f'base_dir:{self.base_dir},categories:{self.categories},images:{[image for image in self.images]}'
 
